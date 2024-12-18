4055f78df55a2f8c1cc0c533def71d5a92eb479f
Román Ríos <romanrios@live.com>
2024-12-18 07:48:54 -0300

    dist

--- v2/vite.config.js
+++ v2/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/https://romanrios.github.io/MTESS-Form/v2/',
   plugins: [react()],
 })
