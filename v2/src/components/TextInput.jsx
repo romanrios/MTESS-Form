3a097367d7224b69e9c5e2ca79fa57395c6a1471
Román Ríos <romanrios@live.com>
2024-12-20 10:37:01 -0300

    dist

--- v2/src/components/TextInput.jsx
+++ v2/src/components/TextInput.jsx
@@ -1,6 +1,6 @@
 export const TextInput = (props) => {
     return <div className="form-group">
         <label>{props.label}</label>
-        <input type="text" id="props.input_id" className="input" />
+        <input className="input" type={props.text} name={props.name} defaultValue={props.defaultValue} />
     </div>
 }
