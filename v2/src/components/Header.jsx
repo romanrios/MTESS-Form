c5425e7ce8d00cb84e595a5e7b02a8bccb9c6ad4
Román Ríos <romanrios@live.com>
2024-12-26 11:57:54 -0300

    header route

--- v2/src/components/Header.jsx
+++ v2/src/components/Header.jsx
@@ -1,7 +1,7 @@
 import '../css/Header.css'
 
 export const Header = () => {
-    return <header className='Header' style={{ backgroundImage: `url('../assets/header_impulsa.jpg')` }}>
+    return <header className='Header' style={{ backgroundImage: `url('./assets/header_impulsa.jpg')` }}>
         <img src="./assets/impulsa_blanco.svg" alt="Logo de Impulsa"></img>
         <h1>Formulario de Presentación de Proyecto</h1>
     </header>
